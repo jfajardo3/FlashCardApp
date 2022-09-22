f6922db9b2ff985e3dabfbdc3bb2f207ce5884ba
jfajardo3 <89419276+jfajardo3@users.noreply.github.com>
2022-09-22 14:19:33 -0500

    final flixster

---
+++ Flixster2/app/src/main/java/com/example/flixster2/MovieAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.flixster2
+
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.imageView)
+        private val tvTitle = itemView.findViewById<TextView>(R.id.textView)
+        private val tvOverview = itemView.findViewById<TextView>(R.id.textView2)
+
+        fun bind(movie: Movie){
+            tvTitle.text = movie.title
+            tvOverview.text = movie.overview
+            Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+
+        }
+    }
+}
